c1a9319fc39f41715b2186796e707995a37851c4
MirkoLisanin <mirkolisanin@gmail.com>
2018-09-11 19:48:15 +0200

    Conection to database and populate Index Action with List of Products

---
+++ WM_TestProject/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WM_TestProject.Repository;
+
+namespace WM_TestProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_repository.GetAllProducts());
+        }
+    }
+}
